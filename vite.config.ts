92c40f97e38c021e3c79a332c5b32d1a8f88c3f8
Ivan Monichev <risen15@yandex.ru>
2024-10-01 00:09:07 +0300

    feat: add ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/scopus-seeker/',
   server: {
     host: true,
     port: 3000
@@ -13,6 +13,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
-    },
-  },
+    }
+  }
 })
